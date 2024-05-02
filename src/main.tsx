5e600082f9acaba2555567982470c2dc610b364e
ArtiomKochkin <artiom.kochkin@yandex.ru>
2024-05-02 22:57:12 +0700

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import "@/app/styles/index.scss";
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container!);
+
+root.render(
+	<div>Notes</div>
+)
