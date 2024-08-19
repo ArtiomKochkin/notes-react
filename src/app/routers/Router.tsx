5c2704421ca5a602dd3cc373db4696a285332c49
ArtiomKochkin <artiom.kochkin@yandex.ru>
2024-08-19 21:20:00 +0700

    upd: add lazy loading for pages

--- src/app/routers/Router.tsx
+++ src/app/routers/Router.tsx
@@ -1,8 +1,10 @@
-import { LabelPage } from "@/pages/label";
-import { MainPage } from "@/pages/main";
-import { ISidebarItemComponent } from "@/shared/types";
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import { ISidebarItemComponent } from "@/shared/types";
+import { Loading } from "@/shared/ui";
+
+const LabelPage = lazy(() => import("@/pages/label").then(module => ({ default: module.LabelPage})));
+const MainPage = lazy(() => import("@/pages/main").then(module => ({ default: module.MainPage})));
 
 interface RouterProps {
     items: ISidebarItemComponent[]
@@ -11,16 +13,18 @@ interface RouterProps {
 export const Router = ({ items }: RouterProps) => {
  
     return (
-        <Routes>
-            {items.map(item => 
-                <Route 
-                    element={React.createElement(item.component)} 
-                    path={item.link} 
-                    key={item.name}
-                />
-            )}
-            <Route element={<LabelPage/>} path="/label"/>
-            <Route element={<MainPage/>} path="*"/>
-        </Routes>
+        <Suspense fallback={<Loading>Загрузка...</Loading>}>
+            <Routes>
+                {items.map(item => 
+                    <Route 
+                        element={React.createElement(item.component)} 
+                        path={item.link} 
+                        key={item.name}
+                    />
+                )}
+                <Route element={<LabelPage/>} path="/label"/>
+                <Route element={<MainPage/>} path="*"/>
+            </Routes>
+        </Suspense>
     )
 }
--- src/shared/const/sidebar.tsx
+++ src/shared/const/sidebar.tsx
@@ -1,12 +1,16 @@
+/* eslint-disable react-refresh/only-export-components */
+
 import { BsTrash } from "react-icons/bs";
 import { MdOutlineLabel, MdOutlineArchive, MdSearch } from "react-icons/md";
 import { SlNote } from "react-icons/sl";
 import { ISidebarItemComponent } from "../types";
-import { ArchivePage } from "@/pages/archive";
-import { DeletedPage } from "@/pages/deleted";
-import { LabelsPage } from "@/pages/labels";
-import { MainPage } from "@/pages/main";
-import { SearchPage } from "@/pages/search";
+import { lazy } from "react";
+
+const ArchivePage = lazy(() => import("@/pages/archive").then(module => ({ default: module.ArchivePage})));
+const DeletedPage = lazy(() => import("@/pages/deleted").then(module => ({ default: module.DeletedPage})));
+const LabelsPage = lazy(() => import("@/pages/labels").then(module => ({ default: module.LabelsPage})));
+const MainPage = lazy(() => import("@/pages/main").then(module => ({ default: module.MainPage})));
+const SearchPage = lazy(() => import("@/pages/search").then(module => ({ default: module.SearchPage})));
 
 export const SIDEBAR_ITEMS: ISidebarItemComponent[] = [
     {
